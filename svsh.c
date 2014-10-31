546a5bf1f5fb7d665b2fc72f8f04515ad6d05d9d
hryo224 <hampton.young@uky.edu>
2014-10-31 14:35:54 -0400

    Added the built in commands

--- svsh.c
+++ svsh.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 #include "svsh_structs.h"
 #include "svsh.h"
 #include "parser.tab.h"
@@ -14,10 +15,26 @@ void printCommandPrompt(void){
 	printf("%s", prompt); 
 }
 void builtInCmd(int command, char* string, char* variable){
+	char cwd[1024]; 
 	switch(command){
 		case(BYE):
 			exit(0);
 			break; 
+		case(DEFPROMPT):
+			prompt = string; 
+			break;
+		case(CD):
+			if(chdir(string) < 0)
+				perror("cd");
+			break;
+		case(LS):
+       			if (getcwd(cwd, sizeof(cwd)) != NULL)
+           			fprintf(stdout, "Current working dir: %s\n", cwd);
+       			else
+           			perror("getcwd() error");
+			break; 
 		default: break; 
 	}
 }
+
+
